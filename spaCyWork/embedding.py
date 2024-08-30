00afa903903b384e8d9b23d96e49bc59289969fa
Ahantya <ahantyasharma57@gmail.com>
2024-08-30 15:26:49 -0500

    saving to hard disk works 1/2 done

--- spaCyWork/embedding.py
+++ spaCyWork/embedding.py
@@ -1,5 +1,6 @@
 import spacy
 import numpy as np
+from spacy.tokens import DocBin, Doc    
 
 
 # step 1: wrap some article into a file that reads this text
@@ -9,18 +10,14 @@ import numpy as np
 # actually average cosine similarities
 
 
-
+nlp = spacy.load("en_core_web_md")
 
             # if similarity > highestSimilarity:
             #     highestSimilarity = similarity
             #     mostSimilarWords = (word1, word2)
 
 
-
-import spacy
-import numpy as np
-
-def getSegment(filename, start, stop):
+def getSpanText(filename, start, stop):
     with open(filename, "r") as f:
         text = f.read()
 
@@ -29,48 +26,80 @@ def getSegment(filename, start, stop):
     segment = ' '.join(segment)
     return segment
 
-def embeddingStore(segment1, segment2):
-    nlp = spacy.load('en_core_web_md')
-    doc1 = nlp(segment1)
-    doc2 = nlp(segment2)
+def saveDocBin(filename, filepath):
+    with open(filename, "r") as f:
+        text = f.read() #redundancy remove for later 
+
+
+    doc = nlp(text)
+    doc_bin = DocBin()
+    doc_bin.add(doc)
+
+    doc_bin.to_disk(filepath)
+
+def loadDocBin(filepath):
+    doc_bin = DocBin().from_disk(filepath)
+
+    docs = list(doc_bin.get_docs(nlp.vocab))
+
+    return docs
+
+
+
+# def embeddingStore(segment1, segment2):
+#     nlp = spacy.load('en_core_web_md')
+#     doc1 = nlp(segment1) # scale this and change this
+#     # multiple paragraphs
+#     doc2 = nlp(segment2) #nlp.pipe instead of this and then store the txt storing spacy docs to disk
     
-    embeddingsList1 = {}
-    embeddingsList2 = {}
+#     embeddingsList1 = {}
+#     embeddingsList2 = {}
 
-    for token in doc1:
-        embeddingsList1[token.text] = token.vector
+#     for token in doc1:
+#         embeddingsList1[token.text] = token.vector
 
-    for token in doc2:
-        embeddingsList2[token.text] = token.vector
+#     for token in doc2:
+#         embeddingsList2[token.text] = token.vector
     
-    return embeddingsList1, embeddingsList2
+    #return embeddingsList1, embeddingsList2
 
-def cosineSimilarity(embedding1, embedding2):
-    dot_product = np.dot(embedding1, embedding2)
-    norm_embedding1 = np.linalg.norm(embedding1)
-    norm_embedding2 = np.linalg.norm(embedding2)
-    return dot_product / (norm_embedding1 * norm_embedding2)
+# def cosineSimilarity(embedding1, embedding2):
+#     dot_product = np.dot(embedding1, embedding2)
+#     norm_embedding1 = np.linalg.norm(embedding1)
+#     norm_embedding2 = np.linalg.norm(embedding2)
+#     return dot_product / (norm_embedding1 * norm_embedding2)
 
-def compareSegments(segment1, segment2):
-    embeddingsList1, embeddingsList2 = embeddingStore(segment1, segment2)
-    totalSimilarity = 0
-    pairCount = 0
+# use the lemma 
 
-    for embedding1 in embeddingsList1.values():
-        for embedding2 in embeddingsList2.values():
-            similarity = cosineSimilarity(embedding1, embedding2)
-            totalSimilarity += similarity
-            pairCount += 1
-    if pairCount == 0:
-        return 0
+# def compareSegments(segment1, segment2): # parameters: two ranges, process embeddings from docs and get what i need
+#     embeddingsList1, embeddingsList2 = embeddingStore(segment1, segment2)
+#     totalSimilarity = 0
+#     pairCount = 0
 
-    averageSimilarity = totalSimilarity / pairCount 
-    return averageSimilarity
+#     for embedding1 in embeddingsList1.values():
+#         for embedding2 in embeddingsList2.values():
+#             similarity = cosineSimilarity(embedding1, embedding2)
+#             totalSimilarity += similarity
+#             pairCount += 1
+#     if pairCount == 0:
+#         return 0
+
+#     averageSimilarity = totalSimilarity / pairCount 
+#     return averageSimilarity
 
 def main():
-    segment1 = getSegment("forSpacy.txt", 0, 10)
-    segment2 = getSegment("forSpacy.txt", 200, 210)
-    similarity = compareSegments(segment1, segment2)
-    print(f"Cosine Average Similarity: {similarity:.4f}")
+    # segment1 = getSegment("spaCyWork/forSpacy.txt", 0, 10)
+    # segment2 = getSegment("spaCyWork/forSpacy.txt", 200, 210)
+    # similarity = compareSegments(segment1, segment2)
+    # print(f"Cosine Average Similarity: {similarity:.4f}")
+    filename = "spaCyWork/forSpacy.txt"
+    docbin_filepath = "full_text.spacy"
+    saveDocBin(filename, docbin_filepath)
+
+    loaded_docs = loadDocBin(docbin_filepath)
+    for doc in loaded_docs:
+        print(doc.text)
+
+
 
 main()
